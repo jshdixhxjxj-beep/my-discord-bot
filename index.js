733c09068065ac7274c075bc931c59954deed9d1
jshdixhxjxj-beep <jshdixhxjxj@gmail.com>
2026-08-28 12:15:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
- const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login('MTU0MjY0ODk4NDExMDM2Njg5MA.GlfVtw.PYOhDiX46LW-bHbHXZYm5w8oYSNIC3zTPmR714');
+client.login('MTU0MjY0ODk4NDExMDM2Njg5MA.GrKZLo.e5KjMfiDveNuCVgK4aZ0uBLI2BKp4PQnFeqjBM');
