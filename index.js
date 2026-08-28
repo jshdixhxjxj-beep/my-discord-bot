72a27d59f59e16cf035404cb78c895d9a2d337bc
jshdixhxjxj-beep <jshdixhxjxj@gmail.com>
2026-08-28 12:43:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is alive!')).listen(process.env.PORT || 3000);
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login('MTU0MjY0ODk4NDExMDM2Njg5MA.GrKZLo.e5KjMfiDveNuCVgK4aZ0uBLI2BKp4PQnFeqjBM');
+client.login('MTU0MjY0ODk4NDExMDM2Njg5MA.G4dZBP.viuhGKyvEjG-Ka3gNFJHRgzTyv9Gzsm8CgMic8');
